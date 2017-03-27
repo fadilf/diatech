969bdf953ec4a295429553cde1467637482fd27e
Fadil Eledath <fadilfaizal@gmail.com>
2017-03-27 16:47:46 +0400

    Video playback issue

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ $(document).ready(function(){
     });
     $("#video-button").click(function(){
         $("#video").css("display", "block");
+        $("#video video").get(0).play();
     });
     $("#close-video").click(function(){
+        $("#video video").get(0).pause();
         $("#video").css("display", "none");
     });
     $(".card").click(function(){
